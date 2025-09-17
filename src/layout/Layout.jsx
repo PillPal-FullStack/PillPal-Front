acdba5a173f1be9ea8abb1492683c318adcf014a
irinatiron <irinatirongiurgiu@gmail.com>
2025-09-17 13:52:15 +0200

    feat(NavBar): added logo

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout() {
   return (
     <div>
       <Navbar />
-      <main style={{ padding: "1rem" }}>
+      <main>
         <Outlet />
       </main>
     </div>
--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -1,63 +1,73 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import styles from "./Navbar.module.css"; 
+import styles from "./Navbar.module.css";
+import { CiPill } from "react-icons/ci";
+import { CiCalendarDate } from "react-icons/ci";
+import { CiUser } from "react-icons/ci";
+
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => setIsOpen(!isOpen);
+    const toggleMenu = () => setIsOpen(!isOpen);
 
-  return (
-    <nav className={styles.navbar}>
-      {/* Botón hamburguesa */}
-      <button
-        className={`${styles.hamburger} ${isOpen ? styles.open : ""}`}
-        onClick={toggleMenu}
-      >
-        <span className={styles.bar}></span>
-        <span className={styles.bar}></span>
-        <span className={styles.bar}></span>
-      </button>
+    return (
+        <nav className={styles.navbar}>
 
-      {/* Menú */}
-      <ul className={`${styles.menu} ${isOpen ? styles.open : ""}`}>
-        <li>
-          <NavLink 
-            to="/" 
-            onClick={() => setIsOpen(false)}
-            className={({ isActive }) => isActive ? styles.activeLink : ""}
-          >
-            Inicio
-          </NavLink>
-        </li>
-        <li>
-          <NavLink 
-            to="/create" 
-            onClick={() => setIsOpen(false)}
-            className={({ isActive }) => isActive ? styles.activeLink : ""}
-          >
-            Añadir medicación
-          </NavLink>
-        </li>
-        <li>
-          <NavLink 
-            to="/history" 
-            onClick={() => setIsOpen(false)}
-            className={({ isActive }) => isActive ? styles.activeLink : ""}
-          >
-            Mi historial
-          </NavLink>
-        </li>
-        <li>
-          <NavLink 
-            to="/profile" 
-            onClick={() => setIsOpen(false)}
-            className={({ isActive }) => isActive ? styles.activeLink : ""}
-          >
-            Mi perfil
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
+            {/* Botón hamburguesa */}
+            <button
+                className={`${styles.hamburger} ${isOpen ? styles.open : ""}`}
+                onClick={toggleMenu}
+            >
+                <span className={styles.bar}></span>
+                <span className={styles.bar}></span>
+                <span className={styles.bar}></span>
+            </button>
+
+            {/* Logo */}
+            <div className={styles.logo}>
+                <img src="./public/pillpal.png" alt="Logo" />
+            </div>
+
+            {/* Menú */}
+            <ul className={`${styles.menu} ${isOpen ? styles.open : ""}`}>
+                <li>
+                    <NavLink
+                        to="/"
+                        onClick={() => setIsOpen(false)}
+                        className={({ isActive }) => isActive ? styles.activeLink : ""}
+                    >
+                        Inicio
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/create"
+                        onClick={() => setIsOpen(false)}
+                        className={({ isActive }) => isActive ? styles.activeLink : ""}
+                    >
+                        <CiPill /> Añadir medicación
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/history"
+                        onClick={() => setIsOpen(false)}
+                        className={({ isActive }) => isActive ? styles.activeLink : ""}
+                    >
+                        <CiCalendarDate /> Mi historial
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/profile"
+                        onClick={() => setIsOpen(false)}
+                        className={({ isActive }) => isActive ? styles.activeLink : ""}
+                    >
+                        <CiUser /> Mi perfil
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
 }
