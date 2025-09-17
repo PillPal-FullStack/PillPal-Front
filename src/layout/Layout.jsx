38a5a49c34c93db2b31e7537839a641cdd70d416
irinatiron <irinatirongiurgiu@gmail.com>
2025-09-17 15:20:25 +0200

    feat: created profile page

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./NavBar";
 import Footer from "./Footer";
-import styles from "./Layout.module.css"; // si usas CSS module
+import styles from "./Layout.module.css";
 
 export default function Layout() {
   return (
