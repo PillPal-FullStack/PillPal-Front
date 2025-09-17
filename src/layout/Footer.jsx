664ea19b5ed4c81aabd38e16c93fee62bf124446
irinatiron <irinatirongiurgiu@gmail.com>
2025-09-17 23:12:51 +0200

    style: home icon on navbar

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <p className={styles.text}>
-        © {new Date().getFullYear()} PillPal
+        © {new Date().getFullYear()} PillPal · Tu aliado diario para no olvidar tu medicación. <br />
+        Proyecto desarrollado en el Hackathon de Factoría F5.
       </p>
     </footer>
   );
