b6fd20e4d28716061a8bbb2dd80fe2b9d024cd63
Mio <gusw.0246.lpsyg@gmail.com>
2025-09-18 18:19:26 +0200

    fixed: route

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,34 +1,3 @@
-import { createBrowserRouter, Navigate } from "react-router-dom";
-import Layout from "../layout/Layout";
-import Main from "../pages/Main";
-import Create from "../pages/Create";
-import History from "../pages/History";
-import Profile from "../pages/Profile";
-import Reminders from "../pages/Reminders";
-
-
-const token = localStorage.getItem("token");
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: token ? <Main /> : <Navigate to="/auth/login" />,
-      },
-      { path: "create", element: <Create /> },
-      { path: "history", element: <History /> },
-      { path: "profile", element: <Profile /> },
-      { path: "reminders", element: <Reminders /> }
-    ],
-  },
-]);
-
-export default router;
-
-/*
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../layout/Layout";
 import Main from "../pages/Main";
@@ -36,8 +5,6 @@ import Create from "../pages/Create";
 import History from "../pages/History";
 import Profile from "../pages/Profile";
 import Reminders from "../pages/Reminders";
-import Login from "../pages/auth/Login";
-import Register from "../pages/auth/Register"
 
 export default function AppRouter() {
   return (
@@ -49,11 +16,9 @@ export default function AppRouter() {
           <Route path="/history" element={<History />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/reminders" element={<Reminders />} />
-          <Route path="/auth/login" element={<Login />}/>
-          <Route path="/auth/register" element={<Register />}/>
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-*/
+
